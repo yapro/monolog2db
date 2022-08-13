2af8b22b23348754cf8317250ef013b2b6895d30
Lebedenko Nikolay <totx@ya.ru>
2022-08-13 10:26:31 +0300

    made a lazy connection

--- src/LogRecordHandler.php
+++ src/LogRecordHandler.php
@@ -20,6 +20,11 @@ use const JSON_UNESCAPED_UNICODE;
 
 class LogRecordHandler extends AbstractProcessingHandler
 {
+    private string $dbHost;
+    private string $dbPort;
+    private string $dbName;
+    private string $dbUserName;
+    private string $dbPassword;
     private Connection $connection;
     private string $logFile;
     private string $tableName;
@@ -34,22 +39,34 @@ class LogRecordHandler extends AbstractProcessingHandler
         string $tableName = null
     ) {
         parent::__construct(Logger::NOTICE, true);
+        $this->dbHost = $dbHost;
+        $this->dbPort = $dbPort;
+        $this->dbName = $dbName;
+        $this->dbUserName = $dbUserName;
+        $this->dbPassword = $dbPassword;
+        $this->logFile = $logFileForUnexpectedErrors;
+        $this->tableName = $tableName ?? 'system_log';
         $this->setFormatter(new LogRecordFormatter());
+    }
+
+    public function createNotExistedConnect(): void
+    {
+        if ($this->connection instanceof Connection) {
+            return;
+        }
         // отдельный коннект к отдельной бд потому что:
         // - лучше использовать отдельную бд которую не нужно бэкапить
         // - приложение иногда начинает транзакцию, которая может не закончится и затем пишет в лог, а транзакция ведь
         //   может закончится падением и следовательно никаких логов мы не увидим.
         // https://www.doctrine-project.org/projects/doctrine-dbal/en/2.10/reference/configuration.html
         $this->connection = DriverManager::getConnection([
-            'host' => $dbHost,
-            'port' => $dbPort,
-            'dbname' => $dbName,
-            'user' => $dbUserName,
-            'password' => $dbPassword,
+            'host' => $this->dbHost,
+            'port' => $this->dbPort,
+            'dbname' => $this->dbName,
+            'user' => $this->dbUserName,
+            'password' => $this->dbPassword,
             'driver' => 'pdo_mysql',
         ]);
-        $this->logFile = $logFileForUnexpectedErrors;
-        $this->tableName = $tableName ?? 'system_log';
     }
 
     protected function write(array $record): void
@@ -65,13 +82,14 @@ class LogRecordHandler extends AbstractProcessingHandler
                 $this->writeError(new UnexpectedValueException('Log record has no formatted state'));
                 return;
             }
+            $this->createNotExistedConnect();
             $this->connection->insert($this->tableName, $record['formatted']);
         } catch (Throwable $exception) {
             $this->writeError($record['formatted']);
             $this->writeError($this->getExceptionDetails($exception));
         }
     }
-    
+
     private function getExceptionDetails(Throwable $exception): array
     {
         return [
@@ -83,7 +101,7 @@ class LogRecordHandler extends AbstractProcessingHandler
             'trace' => $exception->getTraceAsString(),
         ];
     }
-    
+
     private function writeError($record)
     {
         file_put_contents(
